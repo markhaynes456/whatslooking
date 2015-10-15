390e12bf0099f68265ebe6f9e6747652d594d447
Mark Haynes <markhaynes456@gmail.com>
2015-10-15 19:28:38 +0100

    Improved Styling, Fixed comment feature

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,9 @@ class Comment < ActiveRecord::Base
   
   belongs_to :advert
 
+  validates :name, :presence => true
+  validates :message, :presence => true
+  validates :advert_id, :presence => true 
+
+
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,7 @@
 class Contact < ActiveRecord::Base
+
+  validates :name, :presence => true
+  validates :email, :presence => true
+  validates :message, :presence => true
+
 end
